bc1ebe79d782f36e554dbe5464652673b41184d8
Oleh Kylymnyk <okilimnik@gmail.com>
2020-11-15 14:10:36 +0200

    added kubernetes client

--- src/shared/com/example/components/kubernetes.clj
+++ src/shared/com/example/components/kubernetes.clj
@@ -1,13 +1,14 @@
 (ns com.example.components.kubernetes
+  (:require [clojure.java.io :as io])
   (:import
    [java.io FileReader]
    [io.kubernetes.client.openapi Configuration]
    [io.kubernetes.client.openapi.apis CoreV1Api]
    [io.kubernetes.client.openapi.models V1Pod]
-   [io.kubernetes.client.util Config KubeConfig ClientBuilder]))
+   [io.kubernetes.client.util KubeConfig ClientBuilder]))
 
-(defn local-init []
-  (let [kubeConfigPath (str (System/getenv "HOME") "/.kube/config")
+(defn init []
+  (let [kubeConfigPath (io/resource "kube/config")
         client (.build (ClientBuilder/kubeconfig (KubeConfig/loadKubeConfig (FileReader. kubeConfigPath))))]
     (Configuration/setDefaultApiClient client)
     (let [api (CoreV1Api.)
@@ -18,14 +19,3 @@
           (doseq [method methods]
             (println method)))
         (println (.getName (.getMetadata item)))))))
-
-(defn init []
-  (Configuration/setDefaultApiClient (Config/defaultClient))
-  (let [api (CoreV1Api.)
-        list (.listPodForAllNamespaces api false "" "" "" (int 5) "" "" (int 3000) false)]
-    (doseq [^V1Pod item (.getItems list)]
-      #_(let [class! (.getClass item)
-              methods (.getMethods class!)]
-          (doseq [method methods]
-            (println method)))
-      (println (.getName (.getMetadata item))))))
