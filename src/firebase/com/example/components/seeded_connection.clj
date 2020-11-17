c6a28ad47b2a4b764128fdb0963258ff98b410f8
Oleh Kylymnyk <okilimnik@gmail.com>
2020-11-17 22:28:10 +0200

    k8s wip

--- src/firebase/com/example/components/seeded_connection.clj
+++ src/firebase/com/example/components/seeded_connection.clj
@@ -5,12 +5,7 @@
    [clojure.core.async :refer [<!!]]
    [com.fulcrologic.rad.database-adapters.key-value :refer [make-key-store]]))
 
-;;
-;; We've got a tiny database so let's seed it every time we refresh
-;; Far less confusing not to have this :on-reload thing - change the seed function and it will be run!
-;; ^{:on-reload :noop}
-;;
 (defstate kv-connections
   "The connection to the database that has just been freshly populated"
-  :start {:main (make-key-store (<!! (new-fire-store "fire" :root "/konserve" :db "hetaira-gcp"))
+  :start {:main (make-key-store (<!! (new-fire-store "FIREBASE" :root "/hetaira" :db "hetaira-dev"))
                                 "hetaira-web" {})})
