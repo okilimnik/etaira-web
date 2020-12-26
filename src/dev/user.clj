07a88a5c7d9d2b555b21da5337e1d5d1c096d591
Oleh Kylymnyk <okilimnik@gmail.com>
2020-12-27 00:06:21 +0200

    updated dev launch

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,17 +1,8 @@
 (ns user
   (:require
-   [clojure.pprint :refer [pprint]]
    [clojure.spec.alpha :as s]
-   [clojure.tools.namespace.repl
-    :as tools-ns
-    :refer [disable-reload! refresh clear set-refresh-dirs]]
    [expound.alpha :as expound]
-   [etaira.components.server :refer [-main]]
-   [taoensso.timbre :as log]))
+   [etaira.components.server :refer [-main]]))
 
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
 (-main)
-;;clj -M:shadow-cljs release app
-;; clj -e "(compile 'com.example.components.server)"
-;;clj -M:app --aliases package --main-class com.example.components.server
-;;java -cp target/hetaira-web.jar clojure.main -m com.example.components.server
