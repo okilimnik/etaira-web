11da8582d448f3fcbadb3985161498e8b216399c
Oleh Kylymnyk <okilimnik@gmail.com>
2021-01-02 23:37:21 +0200

    firebase init fix

--- src/clj/etaira/components/seeded_connection.clj
+++ src/clj/etaira/components/seeded_connection.clj
@@ -3,15 +3,19 @@
    [mount.core :refer [defstate]]
    [konserve-firebase.core :refer [new-firebase-store]]
    [clojure.core.async :refer [<!!]]
+   [etaira.components.config :refer [config]]
    [com.fulcrologic.rad.database-adapters.key-value :refer [make-key-store]])
-  (:import (com.google.firebase FirebaseApp)
-           (com.google.firebase.database FirebaseDatabase)))
+  (:import (com.google.firebase FirebaseApp FirebaseOptions)
+           (com.google.firebase.database FirebaseDatabase)
+           (com.google.auth.oauth2 GoogleCredentials)))
 
 (defstate kv-connections
           "The connection to the database that has just been freshly populated"
           :start
           {:main
-           (do (FirebaseApp/initializeApp)
+           (do (FirebaseApp/initializeApp (.build (doto (FirebaseOptions/builder)
+                                                    (.setCredentials (GoogleCredentials/fromStream ))
+                                                    (.setDatabaseUrl (:url (get config :firebase/config))))))
                (make-key-store (<!! (new-firebase-store {:db   (FirebaseDatabase/getInstance)
                                                          :root "/hetaira-test"}))
                                "hetaira-web" {}))})
