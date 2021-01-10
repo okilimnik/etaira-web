d2e4e288c8cd9b46a19c898c4ab4425e87c62cd6
Oleh Kylymnyk <okilimnik@gmail.com>
2021-01-10 23:57:41 +0200

    fixing farebase creds

--- src/clj/etaira/components/seeded_connection.clj
+++ src/clj/etaira/components/seeded_connection.clj
@@ -2,6 +2,7 @@
   (:require
    [clojure.string :refer [trim-newline]]
    [mount.core :refer [defstate]]
+   [cheshire.core :as json]
    [konserve-firebase.core :refer [new-firebase-store]]
    [clojure.core.async :refer [<!!]]
    [etaira.components.config :refer [config]]
@@ -11,12 +12,22 @@
            (com.google.auth.oauth2 GoogleCredentials)
            (java.io ByteArrayInputStream)))
 
+(defn remove-trailing-newline [raw]
+  (let [parsed (json/parse-string raw true)]
+    (->> (for [key' (keys parsed)]
+           (if (string? (key' parsed))
+             [key' (trim-newline (key' parsed))]
+             [key' (key' parsed)]))
+         (apply concat)
+         (into {})
+         json/generate-string)))
+
 (defstate kv-connections
           "The connection to the database that has just been freshly populated"
           :start
           {:main
            (do (FirebaseApp/initializeApp (.build (doto (FirebaseOptions/builder)
-                                                    (.setCredentials (GoogleCredentials/fromStream (ByteArrayInputStream. (.getBytes (trim-newline (:firebase/config config))))))
+                                                    (.setCredentials (GoogleCredentials/fromStream (ByteArrayInputStream. (.getBytes (remove-trailing-newline (:firebase/config config))))))
                                                     (.setDatabaseUrl (:firebase/database-url config)))))
                (make-key-store (<!! (new-firebase-store {:db   (FirebaseDatabase/getInstance)
                                                          :root "/hetaira-test"}))
