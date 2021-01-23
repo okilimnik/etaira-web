157a0fc3391e153d935e5fd86a1e5bfc4792e0b6
Oleh Kylymnyk <okilimnik@gmail.com>
2021-01-23 20:21:44 +0200

    another firebase fix

--- src/clj/etaira/components/seeded_connection.clj
+++ src/clj/etaira/components/seeded_connection.clj
@@ -1,24 +1,22 @@
 (ns etaira.components.seeded-connection
   (:require
-   [clojure.string :refer [trim-newline replace]]
    [mount.core :refer [defstate]]
-   [cheshire.core :as json]
    [konserve-firebase.core :refer [new-firebase-store]]
    [clojure.core.async :refer [<!!]]
    [etaira.components.config :refer [config]]
    [com.fulcrologic.rad.database-adapters.key-value :refer [make-key-store]])
   (:import (com.google.firebase FirebaseApp FirebaseOptions)
            (com.google.firebase.database FirebaseDatabase)
-           (com.google.auth.oauth2 GoogleCredentials)
-           (java.io ByteArrayInputStream)))
+           (com.google.auth.oauth2 GoogleCredentials)))
 
 (defstate kv-connections
           "The connection to the database that has just been freshly populated"
           :start
           {:main
            (do 
+             (spit "./firebase.json" (:firebase/config config))
              (FirebaseApp/initializeApp (.build (doto (FirebaseOptions/builder)
-                                                    (.setCredentials (GoogleCredentials/fromStream (ByteArrayInputStream. (.getBytes (replace (:firebase/config config) "\n" "\\n")))))
+                                                    (.setCredentials (GoogleCredentials/getApplicationDefault))
                                                     (.setDatabaseUrl (:firebase/database-url config)))))
                (make-key-store (<!! (new-firebase-store {:db   (FirebaseDatabase/getInstance)
                                                          :root "/hetaira-test"}))
