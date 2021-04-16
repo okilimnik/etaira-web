37188f1d3d14bf268835e359c715fde7a1bdafc6
Igor <igor.kylymnyk@gmail.com>
2021-04-16 14:30:19 +0300

    next redaction tokenomics

--- src/cljs/etaira/ui_ico_segments/tokenomics.cljs
+++ src/cljs/etaira/ui_ico_segments/tokenomics.cljs
@@ -1,12 +1,10 @@
 (ns etaira.ui-ico-segments.tokenomics
   (:require
    [com.fulcrologic.fulcro.dom :as dom :refer [div span br h3]]
-   [com.fulcrologic.rad.rendering.semantic-ui.semantic-ui-controls :as sui]
    [com.fulcrologic.semantic-ui.modules.accordion.ui-accordion :refer [ui-accordion]]
-   ["victory" :refer [VictoryBar VictoryChart VictoryAxis VictoryGroup VictoryPie VictoryTheme VictoryStack VictoryLine sampleDataPolar VictoryPolarAxis]]
+   ["victory" :refer [ VictoryPie]]
    [com.fulcrologic.fulcro.algorithms.react-interop :as interop]
-   [taoensso.timbre :as log]
-   [oops.core :refer [oget ocall]]))
+   [oops.core :refer [oget]]))
 
 (def vpie (interop/react-factory VictoryPie))
 
@@ -21,8 +19,6 @@
 
             :icon "blue large chart pie"}
     :y 500
-    :x 35
-    :rad 50
     :content {:content "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
               :style {:color "blue"
                       :font-size "130%"}}}
@@ -34,9 +30,8 @@
                                     :line-height "2"}} "Hetaira Team & Advisors" (br) "600,000RAM"
                            (br))
             :icon "brown large chart pie"}
-    
+
     :y 600
-    :rad 60
     :content {:content "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
               :style {:color "brown"
                       :font-size "130%"}}}
@@ -48,9 +43,8 @@
                                     :line-height "2"}} "Treasury" (br) "300,000RAM"
                            (br))
             :icon "green large chart pie"}
-    
+
     :y 300
-    :rad 30
     :content {:content "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
               :style {:color "green"
                       :font-size "130%"}}}
@@ -62,24 +56,22 @@
                                     :line-height "2"}} "Seed Investors" (br) "200,000RAM"
                            (br))
             :icon "olive large chart pie"}
-    
+
     :y 200
-    :rad 20
     :content {:content "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
               :style {:color "olive"
                       :font-size "130%"}}}
    {:key "private-sale"
     :label "private-sale"
     :y 700
-    :rad 70
     :fill "violet"
     :title {:content (span {:style {:font-size "150%"
                                     :color "violet"
                                     :line-height "2"}} "Private Sale" (br) "700,000RAM"
                            (br))
             :icon "violet large chart pie"}
-    
-    
+
+
     :content {:content "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
               :style {:color "violet"
                       :font-size "130%"}}}
@@ -91,67 +83,50 @@
                                     :line-height "2"}} "Public Sale" (br) "800,000RAM"
                            (br))
             :icon "purple large chart pie"}
-    
+
     :y 800
-    :rad 80
     :content {:content "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
               :style {:color "purple"
-                      :font-size "130%"}} 
-    }])
+                      :font-size "130%"}}}])
 
 (def color-scale
-  ["blue" "brown" "green" "olive" "violet" "purple"]
-;;   [(str (for [item tokenomics-content]
-;;           (get item :color)))]
-  )
-
-(defn chart []
+  (vec (for [item tokenomics-content]
+         (:fill item))))
   
+(defn chart []
   (div {:style {:width 700
-                ;;:marging "200 200 0 0"
                 :align "center"}}
-   (vpie {:data tokenomics-content
-          :height 200
-          :width 200
-          ;;:max-width "100%"
-          ;;:min-width "100%"
-          :labels (fn [datum] (oget datum :label))
-          :style {:labels {:fontSize "6px"
-                           :fill (fn [datum] "#013220")}
-                  ;;:width "100%"
-                  ;;:marging "100px 0 0 0"
-                  }
-          :colorScale color-scale
-          :cornerRadius (fn [datum] 7)
-          :innerRadius 3
-          :radius (fn [datum] 35)
-          :labelRadius 40
-          :labelPlacement "parallel"
-          :padAngle 1}
-
-        
-        )))
+       (vpie {:data tokenomics-content
+              :height 200
+              :width 200
+              :labels (fn [datum] (oget datum.datum :label))
+              :style {:labels {:fontSize "6px"
+                               :fill (fn [datum] (oget datum.datum :fill))}}
+              :colorScale color-scale
+              :innerRadius 3
+              :radius (fn [datum] (/ (oget datum.datum :y) 20))
+              :labelRadius 45
+              :padAngle 1})))
 
 (def tokenomics (fn []
                   (div :.stackable.ui.grid
-       {:style {:background-color "#00ffff"
-                :width "100%"
-                :min-height "800px"
-                :padding "100px 0"}}
-       (div :.one.column.centered.row
-            (h3 {:style
-                 {:color "#013220"
-                  :font-size "250%"}}
-                "Tokenomics"))
-      ;; (div :.ui.grid
-            (div :.two.column.centered.row
-                 (div :.six.wide.column
-                      (ui-accordion {:defaultActiveIndex "0"
-                                     :panels tokenomics-content
-                                     :exclusive "false"}))
-                 (div :.six.wide.left.aligned.column
-                      {:style {:margin "-100px -50px 0 0"}}
-                      (chart))))))
+                       {:style {:background-color "#00ffff"
+                                :width "100%"
+                                :min-height "800px"
+                                :padding "100px 0"}}
+                       (div :.one.column.centered.row
+                            (h3 {:style
+                                 {:color "#013220"
+                                  :font-size "250%"}}
+                                "Tokenomics"))
+                       (div :.two.column.centered.row
+                            (div :.six.wide.column
+                                 (ui-accordion {:defaultActiveIndex "0"
+                                                :panels tokenomics-content
+                                                :exclusive "false"}))
+                            (div :.six.wide.left.aligned.column
+                                 {:style {:margin "-100px -50px 0 0"}}
+                                 (chart))))))
 
 
 
