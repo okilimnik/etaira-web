75e0ad9c7cfaf186dcaeb6df0c9f913ca6709e28
Igor <igor.kylymnyk@gmail.com>
2021-04-23 16:35:59 +0300

    whitepaper.cljs in progress

--- src/cljs/etaira/ui_ico_segments/whitepaper.cljs
+++ src/cljs/etaira/ui_ico_segments/whitepaper.cljs
@@ -40,69 +40,44 @@
                        :fontSize "250%"}}
                      "HETAIRA"))
             (div :.four.column.centered.row
-                 
-                 (div :.column 
+
+                 (div :.column
                       (ui-button {:animated "true"
-                             :circular "true"
-                             :fluid "true"
-                             }
-                         (ui-button-content {:content "visible"
-                                             :visible "true"})
-                         (ui-button-content {:content "hidden"
-                                             :hidden "true"})))
-                 
-                 (div :.column 
+                                  :circular "true"
+                                  :fluid "true"}
+                                 (ui-button-content {:content "visible"
+                                                     :visible "true"})
+                                 (ui-button-content {:content "hidden"
+                                                     :hidden "true"})))
+
+                 (div :.column
                       (ui-button {:animated "true"
-                             :circular "true"
-                             :fluid "true"
-                                                              }
-                            (ui-button-content {:content "visible"
-                                                :visible "true"})
-                            (ui-button-content {:content "hidden"
-                                                :hidden "true"})))
-                 
-                 )
-            ))
-  )
+                                  :circular "true"
+                                  :fluid "true"}
+                                 (ui-button-content {:content "visible"
+                                                     :visible "true"})
+                                 (ui-button-content {:content "hidden"
+                                                     :hidden "true"})))))))
 
 (defn white-paper-b []
   (div   {:style {:backgroundColor "#00ffff"
                   :width "100%"
-                     ;;:minHeight "800px"
-                  ;;:textAlign "center"
-                     :display "block"
-                     ;;:justifyContent "space-between"
-                     ;;:position "relative"
+                  :display "block"
                   :padding "100px 0"}}
          (ui-grid
-          {:stackable true
-           ;;:centered "true"
-           ;;:padded "true"
-           ;;:inverted "true"
-           ;;:stretched "true"
-           ;;:columns 1
-           ;;:divided "true"
-           }
+          {:stackable true}
           (ui-grid-row
            {:columns 1
-            ;;:centered "true"
-            :textAlign "center"
-            }
+            :textAlign "center"}
            (ui-grid-column
             {:children
              (img {:src "/css/themes/default/assets/images/hetaira2.jpg"
-
                    :style {:width "17%"
-                          ;;:position "relative"
-                           :opacity 0.4
-
-                           ;;:padding "50px 25px"
-                           }})}))
+                           :minHeight "100%"
+                           :opacity 0.4}})}))
           (ui-grid-row
            {:columns 1
-            :textAlign "center"
-           ;;:divided "true"
-            }
+            :textAlign "center"}
            (ui-grid-column
             {:children (h2 {:style
                             {:color "#013220"
@@ -119,26 +94,49 @@
              :children (a {:href "https://google.com"}
                           (ui-button {:animated true
                                       :circular true
-                                   ;;:fluid "true"
+                                      :fluid true
                                       :color "green"
-                                   ;;:font-size "15px"
-                                   ;;:floated "right"
                                       :size "huge"}
-                                  (ui-button-content {:content (span {:style {:color "#013220"}} "Whitepaper")
-                                                      :visible true})
-                                  (ui-button-content {:content (span {:style {:color "#013220"}} "Click please")
-                                                      :hidden true})))})
+                                     (ui-button-content {:content (span {:style {:color "#013220"}} "Whitepaper")
+                                                         :visible true})
+                                     (ui-button-content {:content (span {:style {:color "#013220"}} "Click please")
+                                                         :hidden true})))})
            (ui-grid-column
             {:width 2
              :textAlign "center"
-             :children (ui-button {:animated true
-                                   :circular true
-                                   ;;:fluid "true"
-                                   :color "green"
-                                   :size "huge"}
-                                  (ui-button-content {:content (span {:style {:color "#013220"}} "Documentation")
-                                                      :visible true})
-                                  (ui-button-content {:content (span {:style {:color "#013220"}} "Read more")
-                                                      :hidden true}))}))
-          
-          )))
+             :children (a {:href "https://google.com"}
+                          (ui-button {:animated true
+                                      :circular true
+                                      :fluid true
+                                      :color "green"
+                                      :size "huge"}
+                                     (ui-button-content {:content (span {:style {:color "#013220"}} "Documentation")
+                                                         :visible true})
+                                     (ui-button-content {:content (span {:style {:color "#013220"}} "Read more")
+                                                         :hidden true})))}))
+          (ui-grid-row
+           {:columns 1
+            :centered true
+            :textAlign "center"}
+           (ui-grid-column
+            {:width 4
+             :textAlign "center"
+             :children (div
+                        (a {:href "https://google.com"
+                            :style {:padding "0 20px"}}
+                           (ui-button {:circular true
+                                       :icon "facebook"
+                                       :color "facebook"
+                                       :size "big"}))
+                        (a {:href "https://google.com"
+                            :style {:padding "0 20px"}}
+                           (ui-button {:circular true
+                                       :icon "twitter"
+                                       :color "twitter"
+                                       :size "big"}))
+                        (a {:href "https://google.com"
+                            :style {:padding "0 20px"}}
+                           (ui-button {:circular true
+                                       :icon "linkedin"
+                                       :color "linkedin"
+                                       :size "big"})))})))))
