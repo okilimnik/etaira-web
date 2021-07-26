1fb550c0c862ea144b9eb38f025d247a5bff93fa
Igor <igor.kylymnyk@gmail.com>
2021-07-26 17:24:17 +0300

    state

--- src/cljs/etaira/ui_ico.cljs
+++ src/cljs/etaira/ui_ico.cljs
@@ -1,7 +1,7 @@
 (ns etaira.ui-ico
   (:require
    [etaira.ui-ico-segments.use-cases :refer [use-cases]]
-   [etaira.ui-ico-segments.buy-section :refer [buy-section]]
+   [etaira.ui-ico-segments.buy-section :refer [ui-buy-section ui-rom ui-some-app]]
    [etaira.ui-ico-segments.tokenomics :refer [tokenomics]]
    [etaira.ui-ico-segments.smart-page :refer [smart-page]]
    [etaira.ui-ico-segments.whitepaper :refer [white-paper-b]]
@@ -10,7 +10,9 @@
 
 (defsc MainPage [this props]
   (div
-  (buy-section)
+   (ui-some-app)
+   (ui-rom)
+   (ui-buy-section)
    (white-paper-b)
    (use-cases)
    (smart-page)
--- src/cljs/etaira/ui_ico_segments/buy_section.cljs
+++ src/cljs/etaira/ui_ico_segments/buy_section.cljs
@@ -1,13 +1,15 @@
 (ns etaira.ui-ico-segments.buy-section
   (:require
-   [com.fulcrologic.fulcro.dom :as dom :refer [div span br button h3 p a h2 img i input select option]]
+   [com.fulcrologic.fulcro.dom :as dom :refer [div span br button h4 p a h2 img i label input select option]]
    [com.fulcrologic.semantic-ui.elements.input.ui-input :refer [ui-input]]
    [com.fulcrologic.semantic-ui.modules.dropdown.ui-dropdown :refer [ui-dropdown]]
    [com.fulcrologic.semantic-ui.modules.dropdown.ui-dropdown-item :refer [ui-dropdown-item]]
    [com.fulcrologic.semantic-ui.modules.dropdown.ui-dropdown-search-input :refer [ui-dropdown-search-input]]
    [com.fulcrologic.semantic-ui.modules.dropdown.ui-dropdown-menu :refer [ui-dropdown-menu]]
    [com.fulcrologic.semantic-ui.modules.dropdown.ui-dropdown-divider :refer [ui-dropdown-divider]]
-
+   [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+   [com.fulcrologic.fulcro.mutations :refer [defmutation]]
+   [com.fulcrologic.rad.application :as rad-app]
    [com.fulcrologic.fulcro.algorithms.react-interop :as interop]
    [oops.core :refer [oget]]
    [com.fulcrologic.semantic-ui.elements.button.ui-button :refer [ui-button]]
@@ -17,9 +19,110 @@
    [com.fulcrologic.semantic-ui.collections.grid.ui-grid-row :refer [ui-grid-row]]
    [com.fulcrologic.semantic-ui.elements.image.ui-image :refer [ui-image]]))
 
+(defmutation bump-number [ignored]
+  (action [{:keys [state]}]
+          (swap! state update :rom/numb inc)))
+
+(defsc Rom [this {:rom/keys [numb] :as props} {:keys [visible?]}]
+  {:query         [:rom/numb]
+   :initial-state {:rom/numb 0}
+;;    even we put initial state :rom/numb 2, it is starting with 0. why?????????
+   :ident               (fn [] [:component/id ::Rom])}
+  (dom/div
+   (dom/h4 "This is an example.")
+   (dom/button {:onClick #(comp/transact! this [(bump-number {})])}
+               "You've clicked this button " numb " times.")))
+
+ (def ui-rom (comp/factory Rom))
+
+(defsc SomeApp [this props]
+  
+  (div
+   (ui-rom {:rom/numb 1})))
+
+(def ui-some-app (comp/factory SomeApp))
 
 
-(defn buy-section []
+
+(defonce pair (atom {:ara 1000
+                     :aeth 1}))
+
+
+
+
+
+(defsc Buy [this {:buy/keys [ra eth]}]
+  (div :.ui.compact.menu
+       (div :.ui.simple.dropdown.item
+            "To buy tokens now"
+            (i :.dropdown.icon)
+            (div :.menu
+                 (div :.ui.action.labeled.input
+                      (label)
+                      (input
+                       {:type "number"
+                        :placeholder eth
+                        })
+                      (select :.ui.compact.dropdown
+                              (option {:value "wei"} "wei")
+                              (option {:value "gwei"} "gwei")
+                              (option {:value "ether"
+                                       :selected true} "ether"))
+                      (div :.ui.button "Confirm"))
+                 (div :.ui.dropdown.horizontal.divider "or")
+                 (div :.ui.action.input
+                      (input
+                       {:type "number"
+                        :placeholder ra
+                        })
+
+
+                                        ;; (input
+                                        ;;  {:type "number"
+                                        ;;   :value 44
+                                        ;;     :on-change #(reset! ara (-> % .-target .-value)
+                                        ;;                       ;;  {:ra (-> % .-target .-value)
+                                        ;;                       ;;       :eth (:ra @pair)}
+                                        ;;                         )
+                                        ;;   })
+                                        ;;  (input
+                                        ;;   {:type "number"
+                                        ;;    :placeholder "amount RA to buy"})
+                      (select :.ui.compact.dropdown
+                                                ;;  (option {:value "weiRA"} "weiRA")
+                                                ;;  (option {:value "gweiRA"} "gweiRA")
+                              (option {:value "RA"} "RA"))
+                      (div :.ui.button "Confirm"))))))
+
+(def ui-buy (comp/factory Buy))
+
+(defsc NBuySection [this props]
+  (div
+   (ui-buy {:buy/ra 2000 :buy/eth 3})))
+
+(def ui-n-buy-section (comp/factory NBuySection))
+
+
+(defn ra-input [val]
+  (ui-input
+   {:type "number"
+    :transparent true
+    :placeholder (:ara @val)
+    :value (:ara @val)
+    :onChange #(reset! val {:ara (-> % .-target .-value)
+                            :aeth (/ (-> % .-target .-value) 1000)})}))
+
+(defn eth-input [value]
+  (ui-input
+   {:type "number"
+    :placeholder (:aeth @value)
+    :value (:aeth @value)
+    :onChange #(reset! value {:aeth (-> % .-target .-value)
+                              :ara (* (-> % .-target .-value) 1000)})}))
+
+(defsc buy-section [this props]
+  ;; (let [pair (atom {:ara 1000
+  ;;                   :aeth 1})]
   (div {:style {:backgroundColor "#00ffff"
                 :width "100%"
                 :display "block"
@@ -40,10 +143,11 @@
                                "To buy tokens now"
                                (i :.dropdown.icon)
                                (div :.menu
-                                    (div :.ui.action.input
-                                         (input 
-                                          {:type "number"
-                                           :placeholder "amount ETH to send"})
+                                    (div :.ui.action.labeled.input
+                                         (div :.ui.label
+                                              {:textAlign "center"}
+                                              "ETH for send")
+                                         (eth-input pair)
                                          (select :.ui.compact.dropdown
                                                  (option {:value "wei"} "wei")
                                                  (option {:value "gwei"} "gwei")
@@ -51,54 +155,41 @@
                                                           :selected true} "ether"))
                                          (div :.ui.button "Confirm"))
                                     (div :.ui.dropdown.horizontal.divider "or")
-                                    (div :.ui.action.input
-                                         (input
-                                          {:type "number"
-                                           :placeholder "amount RA to buy"})
+                                    (div :.ui.action.labeled.input
+                                         (div :.ui.label
+                                              "RA for buy")
+                                         (ra-input pair)
+
+
+                                        ;; (input
+                                        ;;  {:type "number"
+                                        ;;   :value 44
+                                        ;;     :on-change #(reset! ara (-> % .-target .-value)
+                                        ;;                       ;;  {:ra (-> % .-target .-value)
+                                        ;;                       ;;       :eth (:ra @pair)}
+                                        ;;                         )
+                                        ;;   })
+                                        ;;  (input
+                                        ;;   {:type "number"
+                                        ;;    :placeholder "RA to buy"})
                                          (select :.ui.compact.dropdown
                                                 ;;  (option {:value "weiRA"} "weiRA")
                                                 ;;  (option {:value "gweiRA"} "gweiRA")
                                                  (option {:value "RA"} "RA"))
                                          (div :.ui.button "Confirm"))
-                                    )))
-           
-           }
-          
-          )
-         (ui-grid-column)
-         (ui-grid-column)
+                                    (div :.ui.dropdown.horizontal.divider "(1ETH = 1000RA)"))))})
          (ui-grid-column
           {:width 4
            :textAlign "center"
-           :children (ui-dropdown
-                      {:icon {:name "hand point left"
-                              :className "green large"}
-                       :text "buy tokens"}
-                      (ui-dropdown-menu
-                       {:direction "left"}
-                       (ui-dropdown-search-input
-                        {:type "number"
-                         :className "big"
-                         :placeholder "amount RA to buy"})
+           :children (ui-n-buy-section)})
+         (ui-grid-column
+          {:children (ui-rom)})
+         (ui-grid-column
+          {:width 4
+           :textAlign "center"
+           :children (div
+                      (p "ra " (:ara @pair))
+                      (p "eth " (:aeth @pair)))})))))
+                                  ;;  )
 
-                       (ui-dropdown-divider
-                        {:as "string"
-                         :className "ui horizontal divider"}
-                        (span
-                         
-
-                         "OR"))
-                       (ui-dropdown-search-input
-                        {:type "number"
-                         :placeholder "amount ETH to send"})
-                       (ui-dropdown-item
-                        {:text "confirm"
-                         :style {:textAlign "center"
-                                 :color "#013220"
-                                 :backgroundColor "green"
-
-                                 :fontSize "150%"}})))})))
-                                 
-                                 
-                                 
-                                 ))
+(def ui-buy-section (comp/factory buy-section))
